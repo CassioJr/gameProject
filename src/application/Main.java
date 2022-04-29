a7c28cee7260713d3543c8c743fce595e96957b6
CassioJr <cassiojuniorceolin@gmail.com>
2022-04-29 14:51:20 -0300

    Refatorando os arquivos

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,21 +14,19 @@ public class Main extends Application {
 	public static Operacoes op;
 	public static CombateController cc;
 
-	// Este metodo � o primeiro a ser executado, quando o programa come�a a rodar
-	// onde mostra a tela de menu
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			// Fun��o onde se coloca um titulo para o programa
+			// Função onde se coloca um titulo para o programa
 			primaryStage.setTitle("Aqui vai o nome do jogo kkkkkkkk");
 
-			// Fun��o onde indica que a tela do programa n�o pode ser redimensionada
+			// Função onde indica que a tela do programa não pode ser redimensionada
 			primaryStage.setResizable(false);
 
-			// Esse codigo serve pra bota icone no aplicativo
+			// Esse codigo serve para colocar icone no aplicativo
 			// stage.getIcons().add(new Image("file:imagens//dk.jpg"));
 
-			// Fun��o � onde se chama a tela de menu
+			// Função onde se chama a tela de menu
 			AnchorPane fxmlmenu = (AnchorPane) FXMLLoader.load(getClass().getResource("/view/TelaMenu.fxml"));
 			Scene main = new Scene(fxmlmenu);
 			primaryStage.setScene(main);
@@ -39,7 +37,7 @@ public class Main extends Application {
 	}
 
 	/*
-	 * Metodo para que n�o precise instanciar toda hora a classe de
+	 * Metodo para que não precise instanciar toda hora a classe de
 	 * MetodosAuxiliares
 	 */
 	public static MetodosAuxiliares instancia() {
@@ -49,7 +47,7 @@ public class Main extends Application {
 		return ma;
 	}
 
-	/* Metodo para que n�o precise instanciar toda hora a classe de Operacoes */
+	/* Metodo para que não precise instanciar toda hora a classe de Operacoes */
 	public static Operacoes operacoes() {
 		if (op == null) {
 			op = new Operacoes();
--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -11,56 +11,41 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import utils.FileManagement;
 
 public class MenuController implements Initializable {
 	@FXML
-	private Button continuar;
+	private Button cont;
 	private Stage primaryStage;
 
-	/*Metodo que chama a tela para realizar a tela de cria��o de personagem*/
+	/* Metodo que chama a tela para realizar a tela de criação de personagem */
 	public void newGame(ActionEvent event) throws IOException {
-		// Main.instancia().MusicBackground("create");
-		AnchorPane fxmlCharCreation = (AnchorPane) FXMLLoader.load(getClass().getResource("/telas/TelaCriacaoPersonagem.fxml"));
-		Scene NovoJogo = new Scene(fxmlCharCreation);
-		primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		primaryStage.setScene(NovoJogo);
-		primaryStage.show();
+		changeScene(event, "TelaCriacaoPersonagem");
 	}
-	
-	/*Metodo que chama a tela de espera que serve para continuar o jogo*/
+
+	/* Metodo que chama a tela de espera que serve para continuar o jogo */
 	public void continueGame(ActionEvent event) throws IOException {
 		Main.operacoes().lerArquivo();
-		AnchorPane fxmlCharCreation = (AnchorPane) FXMLLoader.load(getClass().getResource("/telas/TelaEspera.fxml"));
-		Scene NovoJogo = new Scene(fxmlCharCreation);
-		primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		primaryStage.setScene(NovoJogo);
-		primaryStage.show();
+		changeScene(event, "TelaEspera");
 	}
 
-
-	/*Metodo alternativo que realiza o fechamento do programa, inves de o usuario clicar no fechar no bot�o da janela*/
+	/*
+	 * Metodo alternativo que realiza o fechamento do programa, inves de o usuario
+	 * clicar no fechar no botão da janela
+	 */
 	public void exit() {
 		System.exit(0);
 	}
 
-	/*Metodo que verifica se o arquivo de save do usuario existe, ent�o ele deixa visivel o bot�o de contiunuar o save*/
-	public boolean existFile() {
-		File arquivo = new File("./savedata/save.bin");
-		if (arquivo.exists()) {
-			continuar.setVisible(true);
-			return true;
-		}
-		return false;
-	}
 
 	/*
 	 * Metodo para tocar a musica de background da tela de titulo ela chama um
-	 * metodo da main para n�o ter necessidade de toda hora que chamar ela, a classe
-	 * ser instanciada fazendo assim a musica n�o tocar.
+	 * metodo da main para não ter necessidade de toda hora que chamar ela, a classe
+	 * ser instanciada fazendo assim a musica não tocar.
 	 * O metodo recebe como parametro o nome da musica que deseja que seja tocada
 	 */
 
@@ -68,14 +53,29 @@ public class MenuController implements Initializable {
 		Main.instancia().MusicBackground("Music1");
 	}
 
+	public void changeScene(ActionEvent event, String tela) {
+		try {
+			Parent fxmlCharCreation = FXMLLoader.load(getClass().getResource("/view/" + tela + ".fxml"));
+			primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			primaryStage.setScene(new Scene(fxmlCharCreation));
+			primaryStage.show();
+		} catch (IOException e) {
+			System.out.println("Erro ao carregar a tela!");
+		}
+	}
+
+	public void existSaveFile(){
+		if(FileManagement.existSave() == true)
+		cont.setVisible(true);
+	}
+
 	/*
-	 * Metodo que faz com que assim que o programa inicie chama o metodo de music que faz a musica come�ar
-	 * a tocar, e o metodo Que verifica se existe o arquivo de save para apresentar o bot�o de continuar
+	 * Metodo que faz com que assim que o programa inicie, inicie tambem o que foi definido no seu esqueleto
 	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		playMusic();
-		existFile();
-		Main.operacoes().verificaPastaSave();
+		existSaveFile();
+		FileManagement.SaveDirectoryExists();
 	}
 }
